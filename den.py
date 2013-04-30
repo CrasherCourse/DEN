315e01774ca51cce9c414d84073d840fe5fa5e5f
CrasherCourse <micha454@umn.edu>
2013-04-30 16:37:43 -0500

    functions now use a set of shift keys

--- den.py
+++ den.py
@@ -1,28 +1,38 @@
 # Welcome to den
 # The encoder/decoder
 
-#------------------------------------------
+# Function Defs
+#-----------------------------
 # encode function
 def encode( password ):
+	index = 0
+	keys = [1, 2, 3, 4, 5]
 	outFile = open("test.txt.encoded", "w")
 	for line in inFile:
 		text = ""
 		for c in line.rstrip("\n"):
-			text += str(unichr(ord(c) + 5))
+			shift = keys[index]
+			index = (index+1)%len(keys)
+			text += str(unichr(ord(c) + shift))
 		outFile.write(text + "\n")
 	outFile.close
 	return
 # decode function
 def decode( password ):
+	index = 0
+	keys = [1, 2, 3, 4, 5]
 	outFile = open("test.txt.decoded", "w")
 	for line in inFile:
 		text = ""
 		for c in line.rstrip("\n"):
-			text += str(unichr((ord(c) - 5)))
+			shift = keys[index]
+			index = (index+1)%len(keys)
+			text += str(unichr(ord(c) - shift))
 		outFile.write(text + "\n")
 	outFile.close
 	return
 #--------------------------------------------
+# Main
 inFile = open("test.txt", "r")
 encode(0)
 inFile = open("test.txt.encoded", "r")
