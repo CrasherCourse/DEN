be765b36c94ceac7536ba34334bc82f4dace3eb2
CrasherCourse <micha454@umn.edu>
2013-04-30 16:19:28 -0500

    functions do basic shift cypher correctly

--- den.py
+++ den.py
@@ -1,19 +1,32 @@
 # Welcome to den
 # The encoder/decoder
 
+#------------------------------------------
+# encode function
+def encode( password ):
+	outFile = open("test.txt.encoded", "w")
+	for line in inFile:
+		text = ""
+		for c in line.rstrip("\n"):
+			text += str(unichr(ord(c) + 5))
+		outFile.write(text + "\n")
+	outFile.close
+	return
+# decode function
+def decode( password ):
+	outFile = open("test.txt.decoded", "w")
+	for line in inFile:
+		text = ""
+		for c in line.rstrip("\n"):
+			text += str(unichr((ord(c) - 5)))
+		outFile.write(text + "\n")
+	outFile.close
+	return
+#--------------------------------------------
 inFile = open("test.txt", "r")
-outFile = open("test.txt.output", "w")
-for line in inFile:
-	print line.rstrip("\n")				# removes
-	text = ""
-	for c in line.rstrip("\n"):
-		text += str(unichr(ord(c) + 5))
-	print text
-	outFile.write(text + "\n")
-	decoded = ""
-	for c in text:
-		decoded += str(unichr(ord(c) - 5))
-	print decoded
+encode(0)
+inFile = open("test.txt.encoded", "r")
+decode(0)
 
 inFile.close
-outFile.close
+#--------------------------------------------
