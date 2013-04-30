47e54aeab232217eba7811c11063f8aebd7e110b
CrasherCourse <micha454@umn.edu>
2013-04-30 18:16:11 -0500

    made functions use passwords correctly

--- den.py
+++ den.py
@@ -2,43 +2,62 @@
 # The encoder/decoder
 
 # Function Defs
-#-----------------------------
+#---------------------------------
+# get Shift Keys
+def getShiftKeys( password ):
+	retval = []
+	for ch in password:
+		retval += [ord(ch)]
+	return retval
+
+# character shift
+def charShift( ch, shift):
+	retval = ord(ch)
+	retval += shift
+	if (retval >= 128 ):
+		retval = retval%128
+	if (retval < 0):
+		retval += 128
+	return str(unichr(retval))
+
 # encode function
 def encode( password ):
 	index = 0
-	keys = [1, 2, 3, 4, 5]
+	keys = getShiftKeys( password )
 	outFile = open("test.txt.encoded", "w")
 	for line in inFile:
 		text = ""
 		for c in line.rstrip("\n"):
 			shift = keys[index]
 			index = (index+1)%len(keys)
-			text += str(unichr(ord(c) + shift))
+			text += charShift( c, shift )
 		outFile.write(text + "\n")
 	outFile.close
 	return
+
 # decode function
 def decode( password ):
 	index = 0
-	keys = [1, 2, 3, 4, 5]
+	keys = getShiftKeys( password )
 	outFile = open("test.txt.decoded", "w")
 	for line in inFile:
 		text = ""
 		for c in line.rstrip("\n"):
 			shift = keys[index]
 			index = (index+1)%len(keys)
-			text += str(unichr(ord(c) - shift))
+			text += charShift( c, -(shift) )
 		outFile.write(text + "\n")
 	outFile.close
 	return
 #--------------------------------------------
 # Main
 inFile = open("test.txt", "r")
-encode(0)
-inFile = open("test.txt.encoded", "r")
-decode(0)
-
+encode( "01234" )
 inFile.close
+inFile = open("test.txt.encoded", "r")
+decode( "01234" )
+inFile.close
+
 #--------------------------------------------
 #    OOOOOOO
 #   O\O   O O
